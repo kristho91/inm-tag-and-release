88c738f8be17d90d5d0c950fee7e30eab06bf98b
Kris Thomsen <mail@kristhomsen.dk>
2023-01-23 13:27:00 +0100

    Added code and package updates

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const context = github.context
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const commit = core.getInput('commit') || context.sha
+    const tagName = core.getInput('tag_name', {required: true})
+    const releaseName = core.getInput('release_name') || tagName
+    const body = core.getInput('body')
+    const draft = core.getInput('draft') === 'true'
+    const prerelease = core.getInput('prerelease') === 'true'
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+    const r = await octokit.rest.repos.createRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag_name: tagName,
+      target_commitish: commit,
+      name: releaseName,
+      body,
+      draft,
+      prerelease
+    })
+
+    core.setOutput('id', r.data.id.toString())
+    core.setOutput('html_url', r.data.html_url)
+    core.setOutput('upload_url', r.data.upload_url)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
